a6da4bdb1f2da0671b2a6b3c38777d6c5cb5cb12
changhyeon <changhyeon.h@kakao.com>
2023-04-18 23:49:51 +0900

    fix: SpringConfig

--- src/main/java/spring/practice/SpringConfig.java
+++ src/main/java/spring/practice/SpringConfig.java
@@ -1,2 +1,22 @@
-package spring.practice;public class SpringConfig {
+package spring.practice;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import spring.practice.repository.MemberRepository;
+import spring.practice.repository.MemoryMemberRepository;
+import spring.practice.service.MemberService;
+
+@Configuration
+public class SpringConfig {
+    /*
+    @Bean
+    public MemberService memberService(){
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository(){
+        return new MemoryMemberRepository();
+    }
+*/
 }
