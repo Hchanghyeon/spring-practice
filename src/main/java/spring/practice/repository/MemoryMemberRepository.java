1ac0d633c52caf5a21987434dd1881a88d623131
changhyeon <changhyeon.h@kakao.com>
2023-04-21 01:57:19 +0900

    Feat: H2 Jdbc Connect

--- src/main/java/spring/practice/repository/MemoryMemberRepository.java
+++ src/main/java/spring/practice/repository/MemoryMemberRepository.java
@@ -6,7 +6,6 @@ import spring.practice.domain.Member;
 import java.util.*;
 import java.util.function.IntBinaryOperator;
 
-@Repository
 public class MemoryMemberRepository implements MemberRepository {
     private static Map<Long, Member> store = new HashMap<>();
     private static long sequence = 0L;
