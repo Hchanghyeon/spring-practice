8c67d2ef1a60d39ad3bcb9af4e410bd90d297300
changhyeon <changhyeon.h@kakao.com>
2023-04-22 12:37:50 +0900

    Feat: JdbcTemplateRepository Add

--- src/main/java/spring/practice/repository/JdbcMemberRepository.java
+++ src/main/java/spring/practice/repository/JdbcMemberRepository.java
@@ -12,7 +12,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-@Repository
 public class JdbcMemberRepository implements MemberRepository {
 
     private final DataSource dataSource;
